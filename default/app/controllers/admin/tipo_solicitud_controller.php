0591ce6e9c5959e70be1bcd5c08e99961d0f09d0
Javier Leon <jel1284@gmail.com>
2014-10-02 16:25:56 -0430

    agregado mvc tipo solicitudes y caracteristicas

--- default/app/controllers/admin/tipo_solicitud_controller.php
+++ default/app/controllers/admin/tipo_solicitud_controller.php
@@ -22,9 +22,9 @@
  * @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
  * @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
  */
-Load::models('cargo');
+Load::models('tipo_solicitud');
 
-class CargoController extends AdminController {
+class TipoSolicitudController extends AdminController {
 
     /**
      * Luego de ejecutar las acciones, se verifica si la petición es ajax
@@ -38,8 +38,8 @@ class CargoController extends AdminController {
 
     public function index($pagina = 1) {
         try {
-            $cargos = new Cargo();
-            $this->cargos = $cargos->paginate("page: $pagina");
+            $tipo_solicitudes = new Tipo_solicitud();
+            $this->tipo_solicitudes = $tipo_solicitudes->paginate("page: $pagina");
         } catch (KumbiaException $e) {
             View::excepcion($e);
         }
@@ -47,12 +47,12 @@ class CargoController extends AdminController {
 
     public function crear() {
         try {
-            $this->titulo = 'Crear Cargo';
+            $this->titulo = 'Crear Tipo_solicitud';
 
-            if (Input::hasPost('cargo')) {
-                $cargo = new Cargo(Input::post('cargo'));
-                if ($cargo->save()) {
-                    Flash::valid('El Cargo Ha Sido Agregado Exitosamente...!!!');
+            if (Input::hasPost('tipo_solicitud')) {
+                $tipo_solicitud = new Tipo_solicitud(Input::post('tipo_solicitud'));
+                if ($tipo_solicitud->save()) {
+                    Flash::valid('El Tipo_solicitud Ha Sido Agregado Exitosamente...!!!');
                     if (!Input::isAjax()) {
                         return Router::redirect();
                     }
@@ -66,29 +66,29 @@ class CargoController extends AdminController {
     }
 
     public function editar($id) {
-        $this->titulo = 'Editar Cargo';
+        $this->titulo = 'Editar Tipo_solicitud';
         try {
             $id = (int) $id;
 
             View::select('crear');
 
-            $cargo = new Cargo();
-            $this->cargo = $cargo->find_first($id);
+            $tipo_solicitud = new Tipo_solicitud();
+            $this->tipo_solicitud = $tipo_solicitud->find_first($id);
 
-            if ($this->cargo) {//validamos la existencia del recurso.
-                if (Input::hasPost('cargo')) {
-                    if ($cargo->update(Input::post('cargo'))) {
-                        Flash::valid('El Cargo ha sido Actualizado Exitosamente...!!!');
+            if ($this->tipo_solicitud) {//validamos la existencia del recurso.
+                if (Input::hasPost('tipo_solicitud')) {
+                    if ($tipo_solicitud->update(Input::post('tipo_solicitud'))) {
+                        Flash::valid('El Tipo_solicitud ha sido Actualizado Exitosamente...!!!');
                         if (!Input::isAjax()) {
                             return Router::redirect();
                         }
                     } else {
                         Flash::warning('No se Pudieron Guardar los Datos...!!!');
-                        unset($this->cargo); //para que cargue el $_POST en el form
+                        unset($this->tipo_solicitud); //para que cargue el $_POST en el form
                     }
                 }
             } else {
-                Flash::warning("No existe ningun cargo con id '{$id}'");
+                Flash::warning("No existe ningun tipo_solicitud con id '{$id}'");
                 if (!Input::isAjax()) {
                     return Router::redirect();
                 }
@@ -100,25 +100,25 @@ class CargoController extends AdminController {
 
     public function eliminar($id = NULL) {
         try {
-            $rec = new Cargo();
+            $rec = new Tipo_solicitud();
 
             if (is_int($id)) {
 
                 if (!$rec->find_first($id)) {
-                    Flash::warning("No existe ningun cargo con id '{$id}'");
+                    Flash::warning("No existe ningun tipo_solicitud con id '{$id}'");
                 } elseif ($rec->delete()) {
-                    Flash::valid("El cargo <b>{$rec->cargo}</b> ha sido Eliminado...!!!");
+                    Flash::valid("El tipo_solicitud <b>{$rec->tipo_solicitud}</b> ha sido Eliminado...!!!");
                 } else {
-                    Flash::warning("No se Pudo Eliminar el Cargo <b>{$rec->cargo}</b>...!!!");
+                    Flash::warning("No se Pudo Eliminar el Tipo_solicitud <b>{$rec->tipo_solicitud}</b>...!!!");
                 }
             } elseif (is_string($id)) {
                 if ($rec->delete_all("id IN ($id)")) {
-                    Flash::valid("El Cargo <b>{$id}</b> fue Eliminado...!!!");
+                    Flash::valid("El Tipo_solicitud <b>{$id}</b> fue Eliminado...!!!");
                 } else {
-                    Flash::warning("No se Pudieron Eliminar los Cargos...!!!");
+                    Flash::warning("No se Pudieron Eliminar los Tipo_solicituds...!!!");
                 }
-            } elseif (Input::hasPost('cargos_id')) {
-                $this->ids = Input::post('cargos_id');
+            } elseif (Input::hasPost('tipo_solicitudes_id')) {
+                $this->ids = Input::post('tipo_solicitudes_id');
                 return;
             }
         } catch (KumbiaException $e) {
